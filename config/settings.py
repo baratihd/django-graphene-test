90b51aa6597c4536cd1a7102720f9d306727bd91
Hamed Barati <barati1989@gmail.com>
2023-05-30 03:49:23 -0400

    start app ingrediants

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-party apps
+    'graphene_django',
+    
+    # Local apps
+    'ingredients.apps.IngredientsConfig',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +126,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+GRAPHENE = {
+    "SCHEMA": "django_root.schema.schema",
+}
